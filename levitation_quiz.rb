789703dd1736b82df8dcb986a20eedbe8b31966a
NAM NGUYEN <namdevlife@gmail.com>
2019-03-08 04:18:39 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,6 @@
 def levitation_quiz
 	#your code here
-	levitation_quiz do
+	loop do
 	  puts "What is the spell that enacts levitation?"
 	  answer = gets.chomp
 	  break if answer == "Wingardium Leviosa"
